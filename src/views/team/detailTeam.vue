dd6976d2439cb9c1a0f3f4c688a997b4ee287342
eyrad4 <eyrad4@gmail.com>
2019-03-19 10:57:33 +0200

    created new view and add router

---
+++ src/views/team/detailTeam.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="tabs is-medium">
+    <ul>
+      <li class="is-active"><a>Pictures</a></li>
+      <li><a>Music</a></li>
+      <li><a>Videos</a></li>
+      <li><a>Documents</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
