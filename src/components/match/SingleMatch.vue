a5191071b6f9a1627d923840d908eb608b72b2ef
eyrad4 <eyrad4@gmail.com>
2019-03-19 19:10:36 +0200

    added new features to single match component

--- src/components/match/SingleMatch.vue
+++ src/components/match/SingleMatch.vue
@@ -1,20 +1,28 @@
 <template>
   <div class="columns">
       <div class="column has-text-right">
-        <span class="team-name title is-4">{{ match.homeTeam.name }}</span>
+        <span class="team-name title is-5">{{ match.homeTeam.name }}</span>
       </div>
         <div class="column is-2 has-text-centered">
-          <span class="score title is-5">{{ homeTeamScore}}</span>
-          <span class="score title is-5">{{ awayTeamScore }}</span>
+          <span 
+            :class="checkWinner(match.score.winner, 'home')"
+            class="score title is-5"
+          >{{ homeTeamScore}}</span>
+          <span 
+            :class="checkWinner(match.score.winner, 'away')"
+            class="score title is-5"
+          >{{ awayTeamScore }}</span>
+          <div class="match-date subtitle is-6">{{ dateFormat(match.utcDate) }}</div>
         </div>
       <div class="column has-text-left">
-        <span class="team-name title is-4">{{ match.awayTeam.name }}</span>
+        <span class="team-name title is-5">{{ match.awayTeam.name }}</span>
         
       </div>
     </div>
 </template>
 
 <script>
+import dateFormat from '../../mixins/dateFormat/dateFromat.js'
 export default {
   props: {
     match: {
@@ -22,12 +30,25 @@ export default {
       type: Object
     }
   },
+  mixins: [ dateFormat ],
   computed: {
     homeTeamScore() {
       return (this.match.score.fullTime.homeTeam !== null) ? this.match.score.fullTime.homeTeam : '-'
     },
     awayTeamScore() {
       return (this.match.score.fullTime.awayTeam !== null) ? this.match.score.fullTime.awayTeam : '-'
+    }    
+  },
+  methods: {
+    checkWinner(winner, team) {
+      //TODO REFACTOR
+      if (this.match.status == 'FINISHED') {
+        if (team === 'home') {
+          return (this.match.score.winner == 'HOME_TEAM') ? 'has-background-success has-text-white' : 'has-background-danger has-text-white'
+        } else if (team === 'away') {
+          return (this.match.score.winner == 'AWAY_TEAM') ? 'has-background-success has-text-white' : 'has-background-danger has-text-white'
+        }
+      }
     }
   }
 }
@@ -41,13 +62,21 @@ export default {
     box-shadow: 0 2px 3px rgba(10, 10, 10, 0.1), 0 0 0 1px rgba(10, 10, 10, 0.1);
     color: #4a4a4a;
     display: inline-block;;
-    padding: 0.5rem 1rem;
+    padding: 0.3rem 0.8rem;
   }
   .score.title{
     margin-bottom: 0;
   }
   .score:first-child{
-    margin-right: 1rem;
+    margin-right: 0.5rem;
+  }
+  .team-name{
+    margin-top: 0.3rem;
+    display: inline-block;
+  }
+  .match-date{
+    margin-top: 0.5rem !important;
+    font-size: 12px !important;
   }
 </style>
 
