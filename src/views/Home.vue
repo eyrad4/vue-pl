41ce6bbeeb74f59281a430d5765a7989de06f169
eyrad4 <eyrad4@gmail.com>
2019-03-19 02:06:27 +0200

    rename view Standign to Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,9 +3,15 @@
     <div class="container">
       <div class="columns">
         <div class="column is-9">
-          <app-standings :teams="standings"></app-standings>
+          <app-standings 
+            :teams="standings"
+          ></app-standings>
         </div>
-        <div class="column is-3"></div>
+        <div class="column is-3">
+          <app-short-team-info
+            :team="teamSelected"
+          ></app-short-team-info>
+        </div>
       </div>
       
     </div>
@@ -13,12 +19,14 @@
 </template>
 
 <script>
-import { HTTP } from '../common/api.service'
+//import { HTTP } from '../common/api.service'
 import { mapState } from "vuex";
 import Standings from '../components/standings/Standings';
+import ShortTeamInfo from '../components/team/shortTeamInfo';
 export default {
   components: {
-    'app-standings': Standings
+    'app-standings': Standings,
+    'app-short-team-info': ShortTeamInfo
   },
   data() {
     return {
@@ -30,7 +38,8 @@ export default {
   },
   computed: {
     ...mapState({
-      standings: 'standings'
+      standings: 'standings',
+      teamSelected: 'teamSelected'
     })
   }
 }
