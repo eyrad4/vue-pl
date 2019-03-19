f3d1f0e5d382b18ed707bd3997134b76f5ebfcd4
eyrad4 <eyrad4@gmail.com>
2019-03-19 02:07:24 +0200

    changed router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Standings from '@/views/Standings'
+import Home from '@/views/Home'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Standings',
-      component: Standings
+      name: 'Home',
+      component: Home
     }
   ]
 })
